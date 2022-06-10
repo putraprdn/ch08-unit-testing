9b75552415257e8b7eb1bfd8b08662ced7ebe66e
putraperdana138 <80741806+putraprdn@users.noreply.github.com>
2022-06-10 21:09:56 +0700

    Create heroku db URL

--- config/database.js
+++ config/database.js
@@ -21,5 +21,7 @@ module.exports = {
 		database: process.env.DB_NAME,
 		host: process.env.DB_HOST,
 		dialect: process.env.DB_DIALECT,
+		use_env_variable:
+			"postgres://pzxiuvprwboogr:2629f6cdeffc6ec581a5116a711b3a2ecffeb6c47698f8b1bfe874d2d7baa7d7@ec2-34-231-221-151.compute-1.amazonaws.com:5432/d98pcnf5aekrqn",
 	},
 };
